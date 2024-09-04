61c992b1f658a282c7980fd5af3ed82bf55dcf14
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2024-09-04 11:22:24 -0300

    fix: title and description

--- projeto-simples/next.config.mjs
+++ projeto-simples/next.config.mjs
@@ -1,4 +1,44 @@
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        child_process: false,
+        tls: false,
+        readline: false,
+        dgram: false,
+        dns: false,
+        http2: false,
+        module: false,
+        perf_hooks: false,
+        process: false,
+        punycode: false,
+        querystring: false,
+        repl: false,
+        stream: false,
+        string_decoder: false,
+        sys: false,
+        tty: false,
+        url: false,
+        util: false,
+        vm: false,
+        zlib: false,
+      };
+
+      config.plugins.push(new NodePolyfillPlugin());
+      config.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
